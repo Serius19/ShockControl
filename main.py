350fc8a0b1f8e2bbd1d64a3c8df5d3d3b5746eac
Serius19 <michaelklose19@gmail.com>
2022-05-09 16:47:32 +0930

    V1.04

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox
+
 import numpy as np
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
 
 from acceldata import AccelData
 from utilities import save_file, write2txt
@@ -35,6 +42,7 @@ class MainPage(tk.Frame):
             self.btn_import.config(state="disabled")
             self.btn_export_txt.config(state="normal")
             tk.messagebox.showinfo(title="Info", message="File imported successfully!")
+            MainPlot(self, self.controller).update_plot()
 
     def export_txt_click(self):
         file = save_file()
@@ -43,21 +51,46 @@ class MainPage(tk.Frame):
         tk.messagebox.showinfo(title="Info", message="File exported successfully!")
 
 
+class MainPlot(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.controller = controller
+
+        self.fig, self.ax = plt.subplots()
+        self.ax.set(xlabel='time (ms)', ylabel='Acceleration (gn)',
+                    title='Acceleration vs Time')
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side="top", fill="both", expand=True)
+
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self)
+        self.toolbar.update()
+        self.canvas.get_tk_widget().pack(side="top", fill="both", expand=True)
+
+    def update_plot(self):
+        for i in range(0, len(self.controller.channels_accel)):
+            self.ax.plot(self.controller.channels_accel[0], self.controller.channels_accel[i])
+        self.canvas.draw()
+
+
 class App(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         # configure root window
         self.title("Shock Control System")
-        self.geometry("300x250")
-        self.resizable(False, False)
+        # self.geometry("300x250")
+        # self.resizable(False, False)
 
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
-        self.frames = {"MainFrame": MainPage(container, self)}
-        self.frames["MainFrame"].pack(side="top", fill="both", expand=True)
+        self.frames = {"MainFrame": MainPage(container, self), "MainPlot": MainPlot(container, self)}
+        self.frames["MainFrame"].grid(column=0, row=0, padx=5, pady=5)
+        self.frames["MainPlot"].grid(column=1, row=0, padx=5, pady=5)
 
         #############################
         # Variable Controller
